6dd36206e3f5532cbe3fa6ade5003c96402e3cca
jason <justaguynamedjason@gmail.com>
2020-11-04 12:03:30 -0600

    Refactored the integration tests.

--- src/test/java/com/gobstoppers/antwerp/PortfolioRestControllerIntegrationTest.java
+++ src/test/java/com/gobstoppers/antwerp/PortfolioRestControllerIntegrationTest.java
@@ -7,14 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.UUID;
+import java.util.*;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -25,6 +23,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class PortfolioRestControllerIntegrationTest {
 
 	private static final String APPLICATION_JSON_HAL = "application/hal+json";
+	// Represents the portfolio ID
+	private static final UUID ID1 = UUID.fromString("9d48a88c-1bd2-11eb-adc1-0242ac120002");
+	// Represents the portfolio ID
+	private static final UUID ID2 = UUID.fromString("8d48a88c-1bd2-80eb-edc1-0242ac124532");
+	// Portfolio stocks
+	private static final Set<String> STOCKS = new TreeSet<>(Arrays.asList("AMZN", "GOOG", "TSLA", "AAPL"));
+	// Configure the JWT with scopes antwerp_read and antwerp_write
+	private static final JwtRequestPostProcessor JWT = jwt().jwt(jwt -> jwt.claim("scope", "openid antwerp_read antwerp_write"));
+
 
 	@Autowired
 	private MockMvc mvc;
@@ -34,35 +41,35 @@ class PortfolioRestControllerIntegrationTest {
 
 	@Test
 	void addPortfolio() throws Exception {
-		mvc.perform(post("/portfolios/5c89dea1-1c07-11eb-ade1-0242ac120002")
-				.with(jwt().jwt(jwt -> jwt.claim("scope", "openid antwerp_read antwerp_write")))
+		mvc.perform(post("/portfolios/"+ID1.toString())
+				.with(JWT)
 				.content("{\"stocks\":[\"GOOG\",\"AAPL\",\"TSLA\"]}")
 				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andExpect(content().contentType(APPLICATION_JSON_HAL))
-			.andExpect(jsonPath("uuid").value("5c89dea1-1c07-11eb-ade1-0242ac120002"));
+			.andExpect(jsonPath("uuid").value(ID1.toString()));
 	}
 
 	@Test
 	void getPortfolio() throws Exception {
-		repo.save(new Portfolio(UUID.fromString("9d48a88c-1bd2-11eb-adc1-0242ac120002"), new HashSet<>(Arrays.asList("AMZN", "SPOT", "PYPL"))));
+		repo.save(new Portfolio(ID1, STOCKS));
 
-		mvc.perform(get("/portfolios/9d48a88c-1bd2-11eb-adc1-0242ac120002")
-				.with(jwt().jwt(jwt -> jwt.claim("scope", "openid antwerp_read antwerp_write")))
+		mvc.perform(get("/portfolios/"+ID1.toString())
+				.with(JWT)
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(APPLICATION_JSON_HAL))
-				.andExpect(jsonPath("uuid").value("9d48a88c-1bd2-11eb-adc1-0242ac120002"))
-				.andExpect(jsonPath("stocks[0]", is("AMZN")));
+				.andExpect(jsonPath("uuid").value(ID1.toString()))
+				.andExpect(jsonPath("stocks[0]", is("AAPL")));
 	}
 
 	@Test
 	void getPortfolios() throws Exception {
-		repo.save(new Portfolio(UUID.fromString("9d48a88c-1bd2-11eb-adc1-0242ac120002"), new HashSet<>(Arrays.asList("AMZN", "SPOT", "PYPL"))));
-		repo.save(new Portfolio(UUID.fromString("8d48a88c-1bd2-80eb-edc1-0242ac124532"), new HashSet<>(Arrays.asList("GOOG", "AAPL", "TSLA"))));
+		repo.save(new Portfolio(ID1, STOCKS));
+		repo.save(new Portfolio(ID2, STOCKS));
 
 		mvc.perform(get("/portfolios")
-				.with(jwt().jwt(jwt -> jwt.claim("scope", "openid antwerp_read antwerp_write")))
+				.with(JWT)
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(APPLICATION_JSON_HAL))
